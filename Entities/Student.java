6816bc505b5f72e44d370719cf18e744126a5457
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-05-04 09:54:23 +0400

    Student DisplayByName() method is written

--- Entities/Student.java
+++ Entities/Student.java
@@ -4,7 +4,9 @@ package ObjectOrientedProgramming.OOPDemo.Entities;
 import ObjectOrientedProgramming.OOPDemo.Behaviours.StudentInterface;
 import ObjectOrientedProgramming.OOPDemo.Utils.Constants;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Student extends Person implements StudentInterface {
 
@@ -44,4 +46,22 @@ public class Student extends Person implements StudentInterface {
 
         this.setCourseList(tempCourseList);
     }
+
+    public String DisplayByName(){
+        Student student = new Student();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(Constants.DEPARTMENT_SEARCH_NAME);
+        String searchName = scanner.nextLine();
+
+        List<Student> studentsList = new ArrayList<>();
+        for (int i =0; i<studentsList.size();i++){
+            String targetName = String.valueOf(studentsList.get(i));
+            if (searchName.equalsIgnoreCase(targetName)) {
+                System.out.println("Student Id: " + student.getId());
+                System.out.println("Student Name: " + student.getName());
+            }
+        }
+        return searchName + " " + "Details are displayed";
+    }
+
 }

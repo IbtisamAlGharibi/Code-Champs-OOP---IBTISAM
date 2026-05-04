3d71679cdf66a71d1fbf17c28e26ec5c85250131
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-05-04 10:03:46 +0400

    Course DisplayByName() method is written

--- Entities/Course.java
+++ Entities/Course.java
@@ -1,5 +1,11 @@
 package ObjectOrientedProgramming.OOPDemo.Entities;
 
+import ObjectOrientedProgramming.OOPDemo.Utils.Constants;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Course extends ParentEntity {
 
     private String courseCode;
@@ -17,5 +23,23 @@ public class Course extends ParentEntity {
         return "Course{" +
                 "courseCode='" + courseCode + '\'' +
                 '}';
+    }
+
+    public String DisplayByName(){
+        Course course = new Course();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(Constants.COURSE_SEARCH_NAME);
+        String searchName = scanner.nextLine();
+
+        List<Course> courseList = new ArrayList<>();
+        for (int i =0; i<courseList.size();i++){
+            String targetName = String.valueOf(courseList.get(i));
+            if (searchName.equalsIgnoreCase(targetName)) {
+                System.out.println("Course Id: " + course.getId());
+                System.out.println("Course Name: " + course.getName());
+                System.out.println("Course Code: " + course.getCourseCode());
+            }
+        }
+        return searchName + " " + "Details are displayed";
     }
 }
